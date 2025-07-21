33c6e72a4cac9cbc272d6b0ca99ebbc0c47c28bc
Gobidasan M <gobi.py@gmail.com>
2025-07-21 18:40:10 +0530

    Update and rename test2.txt to test2.sql

---
+++ test2.sql
@@ -0,0 +1,15 @@
+create table student(
+sname varchar(100),
+sid int,
+age int
+);
+
+insert into student values
+('gobi',11,20),
+('guhan',12,20),
+('haridoss',13,21),
+('hariprasant',14,20),
+('harish',15,20);
+
+select * from student;
+
